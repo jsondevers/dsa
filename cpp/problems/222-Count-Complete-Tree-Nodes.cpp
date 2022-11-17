fc7cf9c741ccaffd232dc0647cf7768cd0d0f4c7
J <jasondevers@wireless-10-105-49-243.umd.edu>
2022-11-17 13:11:14 -0500

    gitignore

--- cpp/problems/222-Count-Complete-Tree-Nodes.cpp
+++ cpp/problems/222-Count-Complete-Tree-Nodes.cpp
@@ -15,13 +15,13 @@ Design an algorithm that runs in less than O(n) time complexity.
 #include <queue>
 using std::queue;
 struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right)
-        : val(x), left(left), right(right) {}
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
 };
 
 /* My solution, BFS
@@ -29,34 +29,34 @@ Time: O(n)
 Space: O(n) -> size of queue
 */
 class Solution {
-   public:
-    int countNodes(TreeNode *root) {
-        queue<TreeNode *> queue;
+ public:
+  int countNodes(TreeNode *root) {
+    queue<TreeNode *> queue;
 
-        if (root) {
-            queue.push(root);
-        } else {
-            return 0;
-        }
-
-        int complete = 1;
-        while (queue.size() > 0) {
-            int length = queue.size();
-            for (int i = 0; i < length; i++) {
-                TreeNode *curr = queue.front();
-                queue.pop();
-                if (curr->left) {
-                    queue.push(curr->left);
-                    complete++;
-                }
-                if (curr->right) {
-                    queue.push(curr->right);
-                    complete++;
-                }
-            }
-        }
-        return complete;
+    if (root) {
+      queue.push(root);
+    } else {
+      return 0;
     }
+
+    int complete = 1;
+    while (queue.size() > 0) {
+      int length = queue.size();
+      for (int i = 0; i < length; i++) {
+        TreeNode *curr = queue.front();
+        queue.pop();
+        if (curr->left) {
+          queue.push(curr->left);
+          complete++;
+        }
+        if (curr->right) {
+          queue.push(curr->right);
+          complete++;
+        }
+      }
+    }
+    return complete;
+  }
 };
 
 /* Recursive
@@ -64,10 +64,9 @@ Time: O(n)
 Space: O(n) -> size of callstack
 */
 class Solution {
-   public:
-    int countNodes(TreeNode *root) {
-        return root != NULL
-                   ? 1 + countNodes(root->right) + countNodes(root->left)
-                   : 0;
-    }
+ public:
+  int countNodes(TreeNode *root) {
+    return root != NULL ? 1 + countNodes(root->right) + countNodes(root->left)
+                        : 0;
+  }
 };
